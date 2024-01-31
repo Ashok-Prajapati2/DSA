717e4a9da5525a09dd68c6ada0ce459459c6466f
Ashok-Prajapati2 <ak4132592ashok@gmail.com>
2024-01-31 21:31:43 +0530

    user define datatypes

---
+++ userdatatype.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    struct percentage
+    {
+        int per;
+    };
+
+    struct student
+    {
+        int roll;
+        char name;
+        struct percentage *p;
+    };
+
+    struct student Ashok;
+    struct student Ramesh;
+    Ashok.name = 'A';
+    Ashok.roll = 06;
+    Ashok.p = (struct percentage *)malloc(sizeof(struct percentage));
+    Ashok.p->per = 80;
+
+    Ramesh.name = 'R';
+    Ramesh.roll = 07;
+    Ramesh.p->per = 30;
+
+    free(Ashok.p);
+    
+
+    return 0;
+}
