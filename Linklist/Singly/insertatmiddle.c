702a67ae947271bcdf1de6297ecf85fd266aa74c
Ashok-Prajapati2 <ak4132592ashok@gmail.com>
2024-02-11 17:01:26 +0530

    Link List

---
+++ Linklist/Singly/insertatmiddle.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *start = 0;
+
+struct node *CreateNode()
+{
+    struct node *k = (struct node *)malloc(sizeof(struct node));
+    printf("Enter data : ");
+    scanf("%d",&k->data);
+
+    k->next = 0;
+
+    return k;
+}
+void insertAtStart()
+{
+    struct node *k = CreateNode();
+    if (start == 0)
+    {
+        start = k;
+    }
+    else
+    {
+        k->next = start;
+        start = k;
+    }
+};
+
+void insert()
+{
+    struct node *q = CreateNode();
+
+    if (start == 0)
+    {
+        q = start;
+    }
+    else
+    {
+        int d;
+        printf("Enter data where you want to insert : ");
+        scanf("%d", &d);
+
+        struct node *w, *a;
+        w = start;
+        while (w->next->data != d)
+        {
+            w = w->next;
+        }
+
+        a = w->next;
+        w->next = q;
+        q->next = a;
+    }
+}
+
+void Display()
+{
+    struct node *q = start;
+    while (q != 0)
+    {
+        printf("%d ", q->data);
+        q = q->next;
+    }
+
+}
+int main()
+{
+    while (1)
+    {
+        int ch;
+        printf("Enter ch : ");
+        scanf("%d",&ch);
+
+        switch (ch)
+        {
+        case 1:
+            insert();
+            break;
+        case 2:
+            Display();
+            break;
+
+        case 3:
+            insertAtStart();
+            break;
+        case 4:
+            Display();
+            break;
+        default:
+            break;
+        }
+    }
+    return 0;
+}
