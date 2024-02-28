3dd8832b5b10deaef068e06016939b676abbdeff
Ashok-Prajapati2 <ak4132592ashok@gmail.com>
2024-02-28 20:47:40 +0530

    test

--- Basic/userdatatype.c
+++ Basic/userdatatype.c
@@ -2,24 +2,24 @@
 #include <stdlib.h>
 
 int main()
-{
-    struct percentage
     {
+    struct percentage
+        {
         int per;
-    };
+        };
 
     struct student
-    {
+        {
         int roll;
         char name;
-        struct percentage *p;
-    };
+        struct percentage* p;
+        };
 
     struct student Ashok;
     struct student Ramesh;
     Ashok.name = 'A';
     Ashok.roll = 06;
-    Ashok.p = (struct percentage *)malloc(sizeof(struct percentage));
+    Ashok.p = (struct percentage*)malloc(sizeof(struct percentage));
     Ashok.p->per = 80;
 
     Ramesh.name = 'R';
@@ -27,7 +27,7 @@ int main()
     Ramesh.p->per = 30;
 
     free(Ashok.p);
-    
+
 
     return 0;
-}
+    }
